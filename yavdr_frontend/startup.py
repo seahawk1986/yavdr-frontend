82385dd981e5f71ea797715ee4bac9cb50eafb74
Alexander Grothe <seahawk1986@gmx.de>
2025-08-26 23:46:29 +0200

    next attempt for better handling frontend script termination

--- yavdr_frontend/startup.py
+++ yavdr_frontend/startup.py
@@ -11,30 +11,31 @@ FORMAT = "[ %(filename)s:%(lineno)s: %(name)s.%(funcName)s() ] %(message)s"
 def on_keypress(cmd: str): ...
 
 
-async def create_and_publish_controller(config: Config):
+async def create_and_publish_controller(config: Config) -> Controller:
     # create the the Controller, which also publishes the DBus Interface
 
+    controller = Controller(config)
     async with asyncio.TaskGroup():
-        try:
-            controller = await Controller(config)  # type: ignore # noqa: F841  # this unused variable is needed to keep the object alive
-        finally:
-            await controller.quit()
+        controller = await controller  # type: ignore # noqa: F841  # this unused variable is needed to keep the object alive
+        return controller
 
 async def parse_args_and_run():
     args = StartArgumentParser.parse_args()
     logging.basicConfig(level=args.loglevel, format=FORMAT)
     config = load_yaml(args.config)
-    await create_and_publish_controller(config)
-
+    controller = await create_and_publish_controller(config)
+    return controller
 
 def main():
     loop = asyncio.new_event_loop()
-    loop.run_until_complete(parse_args_and_run())
+    controller = loop.run_until_complete(parse_args_and_run())
     logging.info("Startup complete. Press STRG + C to cancel ...")
     try:
         loop.run_forever()
     except KeyboardInterrupt:
         pass
+    finally:
+        loop.run_until_complete(controller.quit())
 
 
 if __name__ == "__main__":
