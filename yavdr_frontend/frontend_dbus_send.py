f8583a3d3c12a59dbcfc2d77ea10ee6d456419b5
Alexander Grothe <seahawk1986@gmx.de>
2025-08-29 01:14:44 +0200

    fix vdr_dbus_send.py

--- yavdr_frontend/frontend_dbus_send.py
+++ yavdr_frontend/frontend_dbus_send.py
@@ -1,17 +1,20 @@
+# TODO: use argparse
+# import argparse
 import asyncio
 import sys
 from yavdr_frontend.config import load_yaml
 from yavdr_frontend.tools import get_bus
 from yavdr_frontend.interfaces.yavdr_frontend_interface import (
     yaVDRFrontendInterface,
-    YAVDR_FRONTEND_INTERFACE_NAME,
+    YAVDR_FRONTEND_BUS_NAME,
 )
 
-
-async def main():
+async def dbus_send():
     config = load_yaml()
     bus = get_bus(config.main.interface_bus)
-    fe = yaVDRFrontendInterface.new_proxy(YAVDR_FRONTEND_INTERFACE_NAME, "/", bus=bus)
+    fe = yaVDRFrontendInterface.new_proxy(
+        YAVDR_FRONTEND_BUS_NAME, "/Controller", bus=bus
+    )
     if len(sys.argv) > 1:
         cmd = getattr(fe, sys.argv[1])
         return await cmd(*sys.argv[2:])
@@ -20,6 +23,11 @@ async def main():
         print("you can call the following methods:")
         print(await fe.dbus_introspect())
 
+def main():
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('cmd')
+    asyncio.run(dbus_send())
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
