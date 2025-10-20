e67e74832e78fcfa457900283d37573aba416253
Alexander Grothe <seahawk1986@gmx.de>
2025-10-20 16:17:53 +0200

    Use  introspection data for frontend_dbus_send.py

--- yavdr_frontend/frontend_dbus_send.py
+++ yavdr_frontend/frontend_dbus_send.py
@@ -1,7 +1,8 @@
 # TODO: use argparse
-# import argparse
+import argparse
 import asyncio
 import sys
+import xml.etree.ElementTree as ET
 from yavdr_frontend.config import load_yaml
 from yavdr_frontend.tools import get_bus
 from yavdr_frontend.interfaces.yavdr_frontend_interface import (
@@ -9,23 +10,49 @@ from yavdr_frontend.interfaces.yavdr_frontend_interface import (
     YAVDR_FRONTEND_BUS_NAME,
 )
 
+
 async def dbus_send():
     config = load_yaml()
     bus = get_bus(config.main.interface_bus)
     fe = yaVDRFrontendInterface.new_proxy(
         YAVDR_FRONTEND_BUS_NAME, "/Controller", bus=bus
     )
-    if len(sys.argv) > 1:
-        cmd = getattr(fe, sys.argv[1])
-        return await cmd(*sys.argv[2:])
-    else:
-        print("usage: {} COMMAND [ARGS]...".format(sys.argv[0]))
-        print("you can call the following methods:")
-        print(await fe.dbus_introspect())
+    if len(sys.argv) == 1:
+        # print(await fe.properties_get_all_dict())
+        root = ET.fromstring(await fe.dbus_introspect())
+        for child in root.findall(
+            f'.//method/..[@name="{YAVDR_FRONTEND_BUS_NAME}.Controller"]'
+        ):
+            for method in child.findall("./method"):
+                method_args = [
+                    arg.attrib["type"]
+                    for arg in method.findall("./arg[@direction='in']")
+                ]
+                result = [
+                    arg.attrib["type"]
+                    for arg in method.findall("./arg[@direction='out']")
+                ]
+                print(
+                    method.get("name"),
+                    "arguments:",
+                    ", ".join(method_args),
+                    "-> response:",
+                    result,
+                )
+        return
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cmd", help="command")
+    parser.add_argument(
+        "arguments", metavar="ARG", nargs="*", type=str, help="arguments"
+    )
+    args = parser.parse_args()
+    cmd = getattr(fe, args.cmd)
+    arguments = args.arguments
+    return await cmd(*arguments)
+
 
 def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('cmd')
     asyncio.run(dbus_send())
 
 
