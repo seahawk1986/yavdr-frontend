785d9dae36cd93cb2fd5e1c079e8c3faa1110468
Alexander Grothe <seahawk1986@gmx.de>
2025-11-13 09:30:15 +0100

    refactor start_desktop.py

--- yavdr_frontend/start_desktop.py
+++ yavdr_frontend/start_desktop.py
@@ -1,50 +1,49 @@
 #!/usr/bin/env python3
-from argparse import ArgumentParser
 import asyncio
-import os.path
-from .tools import strtobool
+import sdbus
+
+from yavdr_frontend.args import StartArgumentParser
+from yavdr_frontend.config import load_yaml
+from yavdr_frontend.tools import get_bus
 from yavdr_frontend.interfaces.yavdr_frontend_interface import (
     yaVDRFrontendInterface,
     YAVDR_FRONTEND_BUS_NAME,
 )
 
-# TODO: find better filename, add a description what this does
+# TODO: find better filename
 
-import sdbus
-
-
-def get_bus(args: list[str] | None):
-    if args:
-        try:
-            sessionbus = strtobool(args[0])
-        except Exception as e:
-            print(e)
-        else:
-            if sessionbus:
-                print("use SessionBus")
-                return sdbus.sd_bus_open_user()
-    return sdbus.sd_bus_open_system()
+# This scripts forwards the absolute path, desktop_id or name of a .desktop file
+# to the DBus interface of yavdr-frontend.
+#
+# The desktop_id is an escaped version of the subfolder and filename in
+# the user directory, the XDG_DATA_DIRS etc.
+# Slashes of the paths in the subfolders are replaced by dashes - e.g. `/usr/share/applications/foo/bar.desktop` has the id `foo-bar.desktop`
 
 
-async def start_desktop(path: str, args: list[str]):
-    bus = get_bus(args)
+async def start_desktop(desktop_entry: str, args: list[str], bus: sdbus.SdBus) -> None:
     fe = yaVDRFrontendInterface.new_proxy(
         YAVDR_FRONTEND_BUS_NAME, "/Controller", bus=bus
     )
-    path = os.path.splitext(os.path.basename(path))[0]
-    await fe.switchto(path)
+    await fe.switchto(desktop_entry)
 
 
 def main():
-    parser = ArgumentParser(description="start .desktop files programmatically")
-    parser.add_argument(
-        "desktop_file", type=str, help="name of full path of a .desktop file"
+    StartArgumentParser.description = "start .desktop files programmatically"
+    StartArgumentParser.add_argument(
+        "desktop_file",
+        type=str,
+        help="full path, desktop_id or name of a .desktop file",
     )
-    parser.add_argument(
+    StartArgumentParser.add_argument(
         "uris", type=list, nargs="*", help="files/URIs passed to the .desktop file"
     )
-    args = parser.parse_args()
-    asyncio.run(start_desktop(args.desktop_file, args.uris))
+    args = StartArgumentParser.parse_args()
+    config = load_yaml(args.config)
+    asyncio.run(
+        start_desktop(
+            args.desktop_file, args.uris, bus=get_bus(config.main.interface_bus)
+        )
+    )
 
 
 if __name__ == "__main__":
